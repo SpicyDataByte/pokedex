df56486ab25b8867b3d4c991bfa3e9e0051c516c
SpicyDataByte <matt@hildebrandt.club>
2025-04-14 13:42:10 -0600

    adding commands

--- repl.go
+++ repl.go
@@ -8,32 +8,6 @@ import (
     "strings"
 )
 
-// func startRepl() {
-// 	reader := bufio.NewScanner(os.Stdin)
-// 	for {
-// 		fmt.Print("Pokedex > ")
-// 		reader.Scan()
-
-// 		words := cleanInput(reader.Text())
-// 		if len(words) == 0 {
-// 			continue
-// 		}
-
-// 		commandName := words[0]
-
-// 		command, exists := getCommands()[commandName]
-// 		if exists {
-// 			err := command.callback()
-// 			if err != nil {
-// 				fmt.Println(err)
-// 			}
-// 			continue
-// 		} else {
-// 			fmt.Println("Unknown command")
-// 			continue
-// 		}
-// 	}
-// }
 func startRepl() {
 	reader := bufio.NewScanner(os.Stdin)
 	for {
@@ -47,9 +21,20 @@ func startRepl() {
 
 		commandName := words[0]
 
-		fmt.Printf("Your command was: %s\n", commandName)
+		command, exists := getCommands()[commandName]
+		if exists {
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Println("Unknown command")
+			continue
+		}
 	}
 }
+
 func cleanInput(text string) []string {
     output := strings.ToLower(text)
     words := strings.Fields(output)
