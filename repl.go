25b06c4848287419116e3627b9bcbabf150e421f
SpicyDataByte <matt@hildebrandt.club>
2025-04-14 13:23:36 -0600

    test cleanInput

--- repl.go
+++ repl.go
@@ -7,3 +7,29 @@ import (
 func startRepl() {
 	fmt.Println("Hello, World!")
 }
+
+func cleanInput(text string) []string {
+    output := strings.ToLower(text)
+    words := strings.Fields(output)
+    return words
+}
+
+type cliCommand struct {
+    name        string
+    description string
+    callback    func() error
+}
+func getCommands() map[string]cliCommand {
+    return map[string]cliCommand{
+        "help": {
+            name:        "help",
+            description: "Displays a help message",
+            callback:    commandHelp,
+        },
+        "exit": {
+            name:        "exit",
+            description: "Exit the Pokedex",
+            callback:    commandExit,
+        },
+    }
+}
