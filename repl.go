7bf1ac7a3f93174225955685124936e47049e276
SpicyDataByte <matt@hildebrandt.club>
2025-04-15 10:16:36 -0600

    updates

--- repl.go
+++ repl.go
@@ -1,14 +1,22 @@
 package main
 
-
 import (
-    "bufio"
-    "fmt"
-    "os"
-    "strings"
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/bootdotdev/pokedexcli/internal/pokeapi"
 )
 
-func startRepl() {
+type config struct {
+	pokeapiClient    pokeapi.Client
+	nextLocationsURL *string
+	prevLocationsURL *string
+	caughtPokemon    map[string]pokeapi.Pokemon
+}
+
+func startRepl(cfg *config) {
 	reader := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("Pokedex > ")
@@ -20,10 +28,14 @@ func startRepl() {
 		}
 
 		commandName := words[0]
+		args := []string{}
+		if len(words) > 1 {
+			args = words[1:]
+		}
 
 		command, exists := getCommands()[commandName]
 		if exists {
-			err := command.callback()
+			err := command.callback(cfg, args...)
 			if err != nil {
 				fmt.Println(err)
 			}
@@ -36,32 +48,53 @@ func startRepl() {
 }
 
 func cleanInput(text string) []string {
-    output := strings.ToLower(text)
-    words := strings.Fields(output)
-    return words
+	output := strings.ToLower(text)
+	words := strings.Fields(output)
+	return words
 }
 
 type cliCommand struct {
-    name        string
-    description string
-    callback    func() error
+	name        string
+	description string
+	callback    func(*config, ...string) error
 }
+
 func getCommands() map[string]cliCommand {
-    return map[string]cliCommand{
-        "help": {
-            name:        "help",
-            description: "Displays a help message",
-            callback:    commandHelp,
-        },
-        "exit": {
-            name:        "exit",
-            description: "Exit the Pokedex",
-            callback:    commandExit,
-        },
-		"jump": {
-            name:        "jump",
-            description: "Jump Description",
-            callback:    commandJump,
-        },
-    }
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"catch": {
+			name:        "catch <pokemon_name>",
+			description: "Attempt to catch a pokemon",
+			callback:    commandCatch,
+		},
+		"inspect": {
+			name:        "inspect <pokemon_name>",
+			description: "View details about a caught Pokemon",
+			callback:    commandInspect,
+		},
+		"explore": {
+			name:        "explore <location_name>",
+			description: "Explore a location",
+			callback:    commandExplore,
+		},
+		"map": {
+			name:        "map",
+			description: "Get the next page of locations",
+			callback:    commandMapf,
+		},
+		"mapb": {
+			name:        "mapb",
+			description: "Get the previous page of locations",
+			callback:    commandMapb,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
 }
